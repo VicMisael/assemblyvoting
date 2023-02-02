20224332adef676f15305673ff68bbb5e09002c6
Victor Misael <misael1805@gmail.com>
2023-02-02 01:21:47 -0300

    cria Handler para os endpoints de associados e Pautas

---
+++ src/main/java/com/example/assemblyvoting/handler/AssociadoHandler.java
@@ -0,0 +1,45 @@
+package com.example.assemblyvoting.handler;
+
+import com.example.assemblyvoting.model.Associado;
+import com.example.assemblyvoting.model.DTO.AssociadoDTO;
+import com.example.assemblyvoting.service.AssociadoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AssociadoHandler {
+
+    private final AssociadoService associadoService;
+
+    public Mono<ServerResponse> insertAssociado(ServerRequest serverRequest) {
+        log.info("AssociadoHandler.InsertAssociado");
+        return serverRequest.bodyToMono(AssociadoDTO.class)
+                .flatMap(associadoDTO -> associadoService.insertAssociado(associadoDTO.toAssociado())
+                        .flatMap(associado -> created(URI.create("/associado/" + associado.getAssociadoId()))
+                                .contentType(MediaType.APPLICATION_JSON).bodyValue(associado)));
+    }
+
+    public Mono<ServerResponse> getAssociados(ServerRequest serverRequest) {
+        return ok().contentType(MediaType.APPLICATION_JSON).body(associadoService.getAssociados(), Associado.class);
+    }
+
+    public Mono<ServerResponse> getAssociadoById(ServerRequest serverRequest) {
+        return associadoService.getAssociadoById(Long.valueOf(serverRequest.pathVariable("id")))
+                .flatMap(body -> ok().contentType(MediaType.APPLICATION_JSON).bodyValue(body))
+                .switchIfEmpty(notFound().build());
+
+
+    }
+}
---
+++ src/main/java/com/example/assemblyvoting/handler/PautaHandler.java
@@ -0,0 +1,49 @@
+package com.example.assemblyvoting.handler;
+
+import com.example.assemblyvoting.model.Associado;
+import com.example.assemblyvoting.model.DTO.PautaDTO;
+import com.example.assemblyvoting.model.Pauta;
+import com.example.assemblyvoting.model.Sessao;
+import com.example.assemblyvoting.service.PautaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
+
+@Component
+@RequiredArgsConstructor
+public class PautaHandler {
+    private final PautaService pautaService;
+
+    public Mono<ServerResponse> insertPauta(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(PautaDTO.class).flatMap(pautaDTO -> pautaService.insertPauta(pautaDTO.toPauta())
+                .flatMap(pauta -> created(URI.create("/pauta/" + pauta.getId())).contentType(MediaType.APPLICATION_JSON).bodyValue(pauta)));
+    }
+
+    public Mono<ServerResponse> iniciaSessao(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(Sessao.class).flatMap(sessao ->
+                pautaService.iniciaSessao(sessao).flatMap(pauta -> ok().contentType(MediaType.APPLICATION_JSON).body(pauta, Pauta.class)));
+    }
+
+    public Mono<ServerResponse> getPautas(ServerRequest serverRequest) {
+        var pautas = pautaService.getLatestPautas();
+        return ok().contentType(MediaType.APPLICATION_JSON).body(pautas, Pauta.class);
+    }
+
+    public Mono<ServerResponse> getPauta(ServerRequest serverRequest) {
+        Long id = Long.valueOf(serverRequest.pathVariable("id"));
+        return pautaService.getPautaById(id)
+                .flatMap(pauta -> ok().contentType(MediaType.APPLICATION_JSON).body(pauta, Pauta.class))
+                .switchIfEmpty(
+                        notFound().build()
+                );
+    }
+
+
+}
