8142484bbfbb47f53f3354f04e067b9c83187b68
Victor Misael <misael1805@gmail.com>
2023-02-02 01:20:35 -0300

    Atualiza migração do banco

--- src/main/resources/db/migration/V1__assembly_voting.sql
+++ src/main/resources/db/migration/V1__assembly_voting.sql
@@ -5,13 +5,17 @@ create schema assembly_voting
     (
         associado_id SERIAL UNIQUE,
         name         VARCHAR(100) NOT NULL,
+        membro_desde timestamp    not null,
         constraint associado_pk PRIMARY KEY (associado_id)
     )
 
     create table pautas
     (
-        pauta_id   SERIAL UNIQUE,
-        name_pauta text,
+        pauta_id       SERIAL UNIQUE,
+        name_pauta     text NOT NULL,
+        descricao      text,
+        horario_inicio timestamp,
+        horario_fim    timestamp,
         constraint pauta_pk PRIMARY KEY (pauta_id)
 
     )
@@ -19,9 +23,9 @@ create schema assembly_voting
     create table votos
     (
         voto_id      SERIAL,
-        associado_id integer,
-        pauta_id     integer,
-        opcao        integer,
+        associado_id integer NOT NULL,
+        pauta_id     integer NOT NULL,
+        opcao        integer NOT NULL,
         constraint voto_pk PRIMARY KEY (voto_id),
         constraint associado_fk FOREIGN KEY (associado_id) references associados (associado_id),
         constraint pauta_fk FOREIGN KEY (pauta_id) references pautas (pauta_id)
