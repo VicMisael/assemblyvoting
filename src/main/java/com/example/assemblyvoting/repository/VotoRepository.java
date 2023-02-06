422f72c83f604c9df174c2b87b73efb218ea00c1
Victor Misael <misael1805@gmail.com>
2023-02-06 00:47:08 -0300

    Adiciona metodo de busca de pauta e associado a interface repository

--- src/main/java/com/example/assemblyvoting/repository/VotoRepository.java
+++ src/main/java/com/example/assemblyvoting/repository/VotoRepository.java
@@ -1,15 +1,25 @@
 package com.example.assemblyvoting.repository;
 
+import com.example.assemblyvoting.model.Associado;
 import com.example.assemblyvoting.model.DTO.VotoDTO;
+import com.example.assemblyvoting.model.Pauta;
 import com.example.assemblyvoting.model.Voto;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 
 public interface VotoRepository {
 
+    /**
+     * Retorna pauta e associados a partir dos Ids fornecidos em Voto, processa em uma só consulta
+     *
+     * @return Tupla contendo ambos, Caso um ou outro não exista, retorna um MonoVazio
+     */
+    Mono<Tuple2<Pauta, Associado>> findPautaAndAssociadoByPautaIdAndAssociadoId(final Voto voto);
 
     /**
      * computa o voto.
+     *
      * @param voto O voto.
      * @return o Objeto completo de voto.
      */
@@ -17,6 +27,7 @@ public interface VotoRepository {
 
     /**
      * Cehca se o voto já foi computado
+     *
      * @param voto o voto
      * @return Se o voto já foi computado ou não
      */
