6246c78312e589e3bf7d112b9885a91fb8292d91
Victor Misael <misael1805@gmail.com>
2023-02-08 23:18:39 -0300

    corrige bug de PautaOuAssociadoInexistentes

--- src/main/java/com/example/assemblyvoting/exception/PautaOuAssociadoInexistentes.java
+++ src/main/java/com/example/assemblyvoting/exception/PautaOuAssociadoInexistentes.java
@@ -1,6 +1,7 @@
 package com.example.assemblyvoting.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class PautaOuAssociadoInexistentes extends ResponseStatusException {
     public PautaOuAssociadoInexistentes() {
